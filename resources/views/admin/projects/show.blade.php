355d40195a8945b0916f52ffcffdb34d59c7bb87
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-09 16:36:55 +0100

    Add - show type in show view with dump

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
     <div class="container">
+            @dump($project->type_id)
             <h1>{{ $project->title }}</h1>
             <figure><img src="{{ $project->image }}" alt=""></figure>
             <p>{{ $project->description }}</p>
-            <button class="btn btn-warning"><a href="{{ route('projects.edit',$project) }}">modifica</a></button>
-            <form action="{{route('projects.destroy',$project)}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="elimina">
-            </form>
+            <div class="col-auto d-flex">
+                <button class="btn btn-warning mt-3"><a class="text-dark text-decoration-none" href="{{ route('projects.edit',$project) }}">modifica</a></button>
+                <form action="{{route('projects.destroy',$project)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input class="btn btn-danger mt-3 mx-2" type="submit" value="elimina">
+                </form>
+            </div>    
     </div>
 @endsection
